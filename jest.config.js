adfd2ea69f5625db349fc9c27146583ff68251da
Petrov Dmitry <dima.krutoo@yandex.ru>
2019-08-23 17:21:23 +0500

    jest cofig improved

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@ module.exports = {
   verbose: true,
   coverageReporters: [
     'json-summary',
-  ]
+    'json',
+    'lcov',
+    'text',
+    'clover',
+  ],
+  setupFiles: [
+    '<rootDir>/jest.init.js',
+  ],
 };
