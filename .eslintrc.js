94d685575bb0ef5bc3b4cca4254afe00d63c3d1f
krutoo <dima.krutoo@yandex.ru>
2021-07-31 10:35:11 +0500

    eslint config fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  plugins: ['jest', '@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   rules: {
     'linebreak-style': ['error', 'unix'],
