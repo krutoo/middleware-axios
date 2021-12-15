fe028687028d2d6ada5c7cf000ce1e95c86d22b2
krutoo <dima.krutoo@yandex.ru>
2021-12-15 17:14:52 +0500

    deps upd
    - remove some configs and deps
    - eslint config fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     'linebreak-style': ['error', 'unix'],
   },
 };
